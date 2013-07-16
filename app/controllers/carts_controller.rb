6b8c0a2486f867c9dc78546bf05028956c2ccfdf
Lewis <lewis.dackam@gmail.com>
2013-07-16 16:13:21 -0700

    updated logo and videos

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,10 @@
 class CartsController < ApplicationController
 	def show
+	begin
 		@cart = Cart.find(params[:id])
+	rescue ActiveRecord::RecordNotFound
+		logger.error "Attempt to access invalid cart #{params[:id]}"
+		redirect_to store_url, notice: 'Invalid cart'
+	end
 	end
 end
