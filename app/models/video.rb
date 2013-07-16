218cce43c490c1d8f979a8f01273c30441f193c1
Lewis <lewis.dackam@gmail.com>
2013-07-16 01:41:51 -0700

    validations added to video

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,16 @@
 class Video < ActiveRecord::Base
   attr_accessible :description, :poster_url, :price, :title, :video_url
+
+  validates :price, :title:, :video_url, presence: true
+
+  validates :price, numericality:{greater_than_or_equal_to: 0.01, message: "Theprice should be a positive number"
+                                allow_nil: false}
+  validates :title, uniqueness: true
+
+  validates :video_url, format: {with: /\.(mp4|ogv|ogg|webm)\Z/i,
+                               message: 'must be a URL for mp4. ogg or webm video'}
+
+  validates :poster_url,  format: {with: /\.(gif|jpg|png)\Z/i,
+                        message: 'must be a URL for GIF, JPG or PNG image '
+                        allow_blank: true}
 end
