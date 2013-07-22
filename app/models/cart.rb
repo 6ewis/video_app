a9efd033ec32f3b197c97f4aafd8d379d529c5cf
Lewis <lewis.dackam@gmail.com>
2013-07-22 11:39:21 -0700

    fix. no need to purchase same video many times

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -5,7 +5,9 @@ has_many :line_items
 	def add_video(video_id)
 		current_item = line_items.find_by_video_id(video_id)
 		if current_item
-			current_item.quantity +=1
+			#current_item.quantity +=1 not very useful for now but could be in future implementation of the app
+		    # dont need to purchase the same video many times
+		     current_item.quantity = 1
 		else
 			current_item = line_items.build(video_id: video_id)
 		end
